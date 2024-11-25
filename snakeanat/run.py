63f3c5e0b95af6d477ec22bc9069100013714bd6
Ali Khan <alik@robarts.ca>
2024-11-25 10:49:40 -0500

    updates to get snakebids 0.13 run script working

--- snakeanat/run.py
+++ snakeanat/run.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 from pathlib import Path
 
-from snakebids.app import SnakeBidsApp
+from snakebids import bidsapp, plugins
+
+app = bidsapp.app(
+    [
+        plugins.SnakemakeBidsApp(Path(__file__).resolve().parent),
+        plugins.BidsValidator(),
+        plugins.Version(distribution="snakeanat"),
+    ]
+)
 
 
 def get_parser():
-    """Exposes parser for sphinx doc generation, cwd is the docs dir"""
-    app = SnakeBidsApp("../")
-    return app.parser
-
-
-def main():
-    app = SnakeBidsApp(Path(__file__).resolve().parent.parent)  # to get repository root
-    app.run_snakemake()
+    """Exposes parser for sphinx doc generation, cwd is the docs dir."""
+    return app.build_parser().parser
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
